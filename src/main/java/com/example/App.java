c9c25a3eebfc3d176a65c451a0756bddf314a454
cmsandhya26 <sandhya.cm2023@vitstudent.ac.in>
2026-02-23 12:30:16 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,5 @@
 public class Program1 {
-public static void main(String[] args) {
-System.out.println("This is Program 1 - Maven Jenkins Demo");
+  public static void main(String[] args) {
+    System.out.println("This is Program 1 - Maven Jenkins Demo");
 }
 }
